9b73067bc5137f092ef3fc305fb3e6b9d28f3bef
Ubuntu <ubuntu@ip-10-0-1-45.us-west-1.compute.internal>
2022-12-13 02:37:29 +0000

    removing dumb manifest

--- transcripting.py
+++ transcripting.py
@@ -3,9 +3,6 @@ import os
 
 def generate_transcript(input_files: list, output_directory: str,try_gpu = False):
     model = whisper.load_model("large-v2")
-    with open(output_directory + 'manifest.txt', 'w') as f:
-        f.write('\n'.join(input_files))
-        f.close()
     ct = 0
     for item in input_files:
         print('transcribing: ', str(ct), ' out of ', len(input_files)) 
