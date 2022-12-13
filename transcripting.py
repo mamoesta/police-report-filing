f210d72c5b0dba7147f00bba517b5fca2bcd7b3f
marty.moesta <mamoesta@gmail.com>
2022-12-12 17:44:27 -0800

    cleaning up

--- transcripting.py
+++ transcripting.py
@@ -5,12 +5,12 @@ def generate_transcript(input_files: list, output_directory: str,try_gpu = False
     with open(output_directory + 'manifest.txt', 'w') as f:
         f.write('\n'.join(input_files))
         f.close()
-    ct = 32
+    ct = 36
     for item in input_files:
         print('transcribing: ', str(ct), ' out of ', len(input_files)) 
         print('file name: ', item)
         res = model.transcribe(item, fp16=False, language = "english")
-        destination = output_directory + item + '.txt'
+        destination = output_directory + str(ct) + '.txt'
         with open(destination, 'w') as f:
             f.write(res['text'])
             f.close()
