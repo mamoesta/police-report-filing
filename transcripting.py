ae4a57dbd62feb1b5f9a164e2389dcd6fda5376c
marty.moesta <mamoesta@gmail.com>
2022-12-12 18:27:33 -0800

    cleaning up transcripting

--- transcripting.py
+++ transcripting.py
@@ -1,4 +1,5 @@
 import whisper
+import os
 
 def generate_transcript(input_files: list, output_directory: str,try_gpu = False):
     model = whisper.load_model("large-v2")
@@ -9,11 +10,11 @@ def generate_transcript(input_files: list, output_directory: str,try_gpu = False
     for item in input_files:
         print('transcribing: ', str(ct), ' out of ', len(input_files)) 
         print('file name: ', item)
-        with open('/home/ubuntu/police-report-filing/completed.txt', 'w') as f:
+        with open('./completed.txt', 'w') as f:
             f.write(item + '\n')
             f.close()
         res = model.transcribe(item, fp16=False, language = "english")
-        destination = output_directory + str(ct) + '.txt'
+        destination = output_directory + os.path.basename(item) + '.txt'
         with open(destination, 'w') as f:
             f.write(res['text'])
             f.close()
